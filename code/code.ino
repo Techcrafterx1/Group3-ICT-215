7aa5f506a1ca71c9b1e2c776b4c4be9d7ed811f6
Techcrafterx1 <abdulmsmart@gmail.com>
2024-12-29 21:50:00 -0800

    Added a sketch for visualization can be editted if nessary, added the code for the arduino, added the components in Project .pdsprj, remaining components are the motor driver, and the camera.

---
+++ code/code.ino
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+#include <avr/io.h>
+
+#include <util/twi.h>
+
+#include <util/delay.h>
+
+#include <avr/pgmspace.h>
+
+//m1 is the direction motor
+//m2 is the power motor
+// L is left 
+// R is right
+// F is forward
+// B is Backward
+
+int m1L = 13; 
+int m1R = 12; 
+int m2F = 11; 
+int m2B = 10; 
+int l1 = 9; //l1 is the green LED
+int l2 = 8; //l2 is the red LED
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(m1L, OUTPUT);
+  pinMode(m1R, OUTPUT);
+  pinMode(m2F, OUTPUT);
+  pinMode(m2B, OUTPUT);
+  pinMode(l1, OUTPUT);
+  pinMode(l2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+
+void steerLeft(){
+    digitalWrite(m1L, 1);
+    digitalWrite(m1R, 0);
+  };
+  
+  void steerRight(){
+    digitalWrite(m1L, 0);
+    digitalWrite(m1R, 1);
+  };
+
+  void goForward() {
+    digitalWrite(m2F, 1);
+    digitalWrite(m2B, 0);
+  };
+  
+  void goBackward() {
+    digitalWrite(m2F, 0);
+    digitalWrite(m2B, 1);
+  };
+
+  void stop() {
+    digitalWrite(m1L, 0);
+    digitalWrite(m1R, 0);
+    digitalWrite(m2F, 0);
+    digitalWrite(m2B, 0);
+  }
